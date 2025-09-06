13d2a1b74375cbd1f45bd642bcfa025609e50228
Sid Potti <sidsoc12@gmail.com>
2025-09-06 13:09:51 -0400

    dt training"

---
+++ cs336_systems/distributed_training/all_reduce_benchmark.py
@@ -0,0 +1,52 @@
+import os
+import torch
+import time
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+
+def setup(rank, world_size, backend):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    # initialize communication for each process
+    dist.init_process_group(backend, rank=rank, world_size=world_size)
+    if backend == "nccl":
+        # map each process to a GPU
+        torch.cuda.set_device(rank)
+
+# ran for each process
+def benchmark_allreduce(rank, world_size, tensor_size_mb, num_iters=20, warmup=5):
+
+    # Initialize each process
+    setup(rank, world_size, backend)
+
+
+    # pick device
+    if backend == "nccl":
+        device = torch.device(f"cuda:{rank}")
+    else:
+        device = torch.device("cpu")
+    
+    num_elements = tensor_size_mb * 1024 * 1024 // 4  # convert MB to number of elements
+
+
+    # warmup
+    for _ in range(warmup):
+        dist.all_reduce(data)
+
+
+
+
+
+
+if __name__ == "__main__":
+    backends = ["gloo", "nccl"]
+    tensor_sizes = [1,10,100,1024]
+    world_sizes = [2,4,6]
+
+    for backend in backends:
+        for ws in world_sizes:
+            for ts in tensor_sizes:
+                # spawn distributed processes
+                mp.spawn(benchmark_allreduce, args=(ws, backend, ts), nprocs = ws, join=True)
+
